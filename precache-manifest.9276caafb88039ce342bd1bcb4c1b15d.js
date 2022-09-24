b4597ce4d6eafba7d0f971f2c69e68617fc2b58e
Sana Munir <sana.munir2902@gmail.com>
2022-09-24 15:33:27 +0500

    Updates

---
+++ precache-manifest.9276caafb88039ce342bd1bcb4c1b15d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "81198a641afe43ac4a8b",
+    "url": "/robofriends/static/css/main.0c43c7ed.chunk.css"
+  },
+  {
+    "revision": "81198a641afe43ac4a8b",
+    "url": "/robofriends/static/js/main.81198a64.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "a75fc234ffe81749bfe0",
+    "url": "/robofriends/static/css/2.0309d2ed.chunk.css"
+  },
+  {
+    "revision": "a75fc234ffe81749bfe0",
+    "url": "/robofriends/static/js/2.a75fc234.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "057369de4e3e2275d702fbe271746c46",
+    "url": "/robofriends/index.html"
+  }
+];
